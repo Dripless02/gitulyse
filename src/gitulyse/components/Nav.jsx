81a9776386a7120390803252a1fbf706f7405d07
Afolabi <afolabi.fatogun2@mail.dcu.ie>
2023-12-28 01:26:26 +0000

    feat: Added placeholder logo to nav bar component

--- src/gitulyse/components/Nav.jsx
+++ src/gitulyse/components/Nav.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+"use client";
+import Link from 'next/link';
+import Image from 'next/image';
+import {useState, useEffect} from 'react';
+// import {signIn, signOut, useSession, getproviders} from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-15 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'>
+            <Image src="/assets/images/logo.png"
+                    alt="gitulyse logo"
+                    width={70}
+                    height={0}
+                    className='object-contain'/>
+            <p className='logo_text'><h1> Gitulyse </h1></p>
+
+        </Link>
+        <div className='sm:flex hidden'>
+
+        </div>
+
+    </nav>
   )
 }
 
