2f212339b2f8d3e6ce277b81a5dab898599af418
Afolabi <afolabi.fatogun2@mail.dcu.ie>
2024-01-10 01:52:35 +0000

    Added Mantine UI to project

--- src/gitulyse/app/layout.jsx
+++ src/gitulyse/app/layout.jsx
@@ -1,26 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import '@mantine/core/styles.css';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 export const metadata = {
-    title: "Gitulyse",
-    description: 'Code reporting and summary'
-}
-const Rootlayout = ({children}) => {
+  title: "Gitulyse",
+  description: "Code reporting and summary",
+};
+const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
-
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>
+          <div className="app">
+            <Nav />
+            {children}
+          </div>
+        </MantineProvider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default Rootlayout;
