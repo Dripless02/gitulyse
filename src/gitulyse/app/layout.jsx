9aa98e5aebea3ba763e242eac1a9dff8f66efccb
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-01-11 17:05:02 +0000

    Add NextAuth config and session provider

--- src/gitulyse/app/layout.jsx
+++ src/gitulyse/app/layout.jsx
@@ -3,23 +3,29 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import SessionProvider from "@components/SessionProvider"
+import { getServerSession } from "next-auth";
 export const metadata = {
   title: "Gitulyse",
   description: "Code reporting and summary",
 };
-const Rootlayout = ({ children }) => {
+async function Rootlayout({ children }) {
+  const session = await getServerSession()
+
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>
-          <div className="app">
-            <Nav />
-            {children}
-          </div>
-        </MantineProvider>
+        <SessionProvider session={session}>
+          <MantineProvider>
+            <div className="app">
+              <Nav />
+              {children}
+            </div>
+          </MantineProvider>
+        </SessionProvider>
       </body>
     </html>
   );
