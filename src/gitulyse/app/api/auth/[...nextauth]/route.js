c26ad0800530c143e147e87ded61a0662f71934d
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-01-22 22:50:42 +0000

    Update auth route to send access token to backend

--- src/gitulyse/app/api/auth/[...nextauth]/route.js
+++ src/gitulyse/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
+const BACKEND_URL = process.env.BACKEND_URL;
+
 export const authOptions = {
     providers: [
         GitHubProvider({
@@ -8,6 +10,20 @@ export const authOptions = {
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
     ],
+    callbacks: {
+        // send access token to the backend
+        async signIn(user, access, profile) {
+            fetch(`${BACKEND_URL}/github/callback`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    access_token: user.account.access_token,
+                }),
+            });
+        },
+    },
 };
 
 export const handler = NextAuth(authOptions);
