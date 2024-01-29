51c240d93acefb4530d6f93e29a759c65a48f341
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-01-29 19:55:48 +0000

    Add mantine charts to package.json

--- src/gitulyse/app/layout.jsx
+++ src/gitulyse/app/layout.jsx
@@ -1,34 +1,36 @@
-import "@styles/globals.css";
 import Nav from "@components/Nav";
-import Provider from "@components/Provider";
-import '@mantine/core/styles.css';
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import SessionProvider from "@components/SessionProvider"
+import SessionProvider from "@components/SessionProvider";
+import "@mantine/charts/styles.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@styles/globals.css";
 import { getServerSession } from "next-auth";
-export const metadata = {
-  title: "Gitulyse",
-  description: "Code reporting and summary",
-};
-async function Rootlayout({ children }) {
-  const session = await getServerSession()
 
-  return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <SessionProvider session={session}>
-          <MantineProvider defaultColorScheme="dark" >
-            <div className="app">
-              <Nav />
-              {children}
-            </div>
-          </MantineProvider>
-        </SessionProvider>
-      </body>
-    </html>
-  );
+export const metadata = {
+    title: "Gitulyse",
+    description: "Code reporting and summary",
 };
+
+async function Rootlayout({ children }) {
+    const session = await getServerSession();
+
+    return (
+        <html lang="en">
+            <head>
+                <ColorSchemeScript />
+            </head>
+            <body>
+                <SessionProvider session={session}>
+                    <MantineProvider defaultColorScheme="dark">
+                        <div className="app">
+                            <Nav />
+                            {children}
+                        </div>
+                    </MantineProvider>
+                </SessionProvider>
+            </body>
+        </html>
+    );
+}
 
 export default Rootlayout;
