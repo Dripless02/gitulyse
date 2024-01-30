40349ca1e55ee67f89e0efade4c0f1fc5e97eba5
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-01-30 22:56:42 +0000

    Update next config to include GitHub avatars

--- src/gitulyse/next.config.js
+++ src/gitulyse/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            // https://avatars.githubusercontent.com/u/38259057?v=4
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
