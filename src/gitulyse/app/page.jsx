f9756d0d97d121cadc2a4a2bda9cee2e72ad9487
Afolabi <afolabi.fatogun2@mail.dcu.ie>
2024-02-08 18:10:24 +0000

    Add marquee effect to homepage

--- src/gitulyse/app/page.jsx
+++ src/gitulyse/app/page.jsx
@@ -3,17 +3,32 @@
 import Repos from "@components/Repos";
 import Search from "@components/Search";
 import { useSession } from "next-auth/react";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     const { status } = useSession();
 
     return (
         <section className="w-full flex-center flex-col">
-            <h1 className="head_text text-center pb-5">
-                Gitulyse
-                <br className="max-md: hidden" />
-                <span className="blue_gradient"> Code Summarization and Reporting Tool</span>
-            </h1>
+            <Marquee speed={100}>
+                <div>
+                    <h1 className="head_text text-center pb-5 bigtext blue_gradient antialiased ">
+                        &nbsp;&nbsp;&nbsp;Gitulyse, Analyse your Git Repositories.&nbsp;&nbsp;&nbsp;
+                    </h1>
+                </div>
+
+                <div>
+                    <h1 className="head_text text-center pb-5 bigtext blue_gradient antialiased ">
+                        &nbsp;&nbsp;&nbsp;Seearch for a repository or user to get started.&nbsp;&nbsp;&nbsp;
+                    </h1>
+                </div>
+
+                <div>
+                    <h1 className="head_text text-center pb-5 bigtext blue_gradient antialiased ">
+                        &nbsp;&nbsp;&nbsp;Find your own Repos below.&nbsp;&nbsp;&nbsp;
+                    </h1>
+                </div>
+            </Marquee>
 
             {status === "authenticated" ? (
                 <>
