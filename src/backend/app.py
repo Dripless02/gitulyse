4dc09b9ebb82eac73fcaa6c7920c40ac57c1a632
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-03-29 20:40:50 +0000

    Store commits in db and retrieve on get-commits get request

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,13 +1,13 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from time import time
-
 
 import pymongo.database
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from github import Auth, Github
+from github import Auth, Github, GithubException
 from pymongo import MongoClient
 
 load_dotenv()
@@ -115,9 +115,10 @@ def parse_commit(commit):
         "message": commit.commit.message,
         "url": commit.html_url,
         "sha": commit.sha,
+        "monthly_key": monthly_key,
     }
 
-    return monthly_key, commit_info
+    return commit_info
 
 
 @app.route("/get-commits", methods=["GET"])
@@ -131,13 +132,39 @@ def get_commits_from_repo():
 
     repo = f"{owner}/{repo_name}"
     repo = g.get_repo(repo)
-    commits = repo.get_commits()
+
+    # each user will have their own database that includes collections of their repos containing information on commits
+    db_repo = db_client[owner][repo_name]
+
+    commits = db_repo.find().sort("timestamp", pymongo.DESCENDING).limit(1)
+    try:
+        last_commit_timestamp = commits[0]["timestamp"]
+    except IndexError:
+        last_commit_timestamp = None
+
+    if last_commit_timestamp is not None:
+        commits = repo.get_commits(since=datetime.fromtimestamp(last_commit_timestamp))
+    else:
+        commits = repo.get_commits()
 
     commit_stats = {"monthly": {}}
 
+    # get all stored commits from the database without the "_id" field
+    stored_commits = list(db_repo.find({}, {"_id": 0}))
+
+    for stored in stored_commits:
+        commit_stats["monthly"].setdefault(stored["monthly_key"], []).append(stored)
+
     with ThreadPoolExecutor(max_workers=20) as executor:
-        for monthly_key, commit_info in executor.map(parse_commit, commits):
-            commit_stats["monthly"].setdefault(monthly_key, []).append(commit_info)
+        for commit_info in executor.map(parse_commit, commits):
+            commit_stats["monthly"].setdefault(commit_info["monthly_key"], []).append(commit_info)
+            db_repo.update_one(
+                {"sha": commit_info["sha"]},
+                {"$set": commit_info},
+                upsert=True
+            )
+
+    app.logger.info(f"\033[96m commit_stats: {commit_stats} \033[0m")
 
     for monthly_key, monthly_commits in commit_stats["monthly"].items():
         total_lines_of_code = sum(commit["lines_of_code"] for commit in monthly_commits)
@@ -148,6 +175,7 @@ def get_commits_from_repo():
             "average_lines_of_code": average_lines_of_code,
             "commits": monthly_commits,
         }
+
     return jsonify(commit_stats)
 
 
@@ -209,6 +237,7 @@ def get_pull_requests():
             pull_request_list.append(pull_request_info)
 
     return jsonify({"pull_requests": pull_request_list})
+
 
 # get issues get request
 @app.route("/get-issues", methods=["GET"])
