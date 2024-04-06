9f9abfb4275ca16501ffce1e792f3a72fabd8cc7
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-04-06 17:16:45 +0100

    Update Nav profile picture to link to user profile

--- src/gitulyse/components/Nav.jsx
+++ src/gitulyse/components/Nav.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Center, TextInput } from "@mantine/core";
+import { TextInput } from "@mantine/core";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,6 +8,7 @@ import Link from "next/link";
 const Nav = () => {
     const { data: session } = useSession();
     const [value, setValue] = useState("");
+
     return (
         <nav className="flex-between w-full mb-15 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
@@ -22,35 +23,32 @@ const Nav = () => {
             </Link>
             <div className="sm:flex items-center">
                 {session ? (
-                    <div className="  flex items-center">
+                    <div className="flex items-center">
+                        <TextInput
+                            className="pt-4 justify-center pr-96 mr-44"
+                            variant="filled"
+                            placeholder="Enter Repo URL..."
+                            size="xl"
+                            radius="60"
+                            value={value}
+                            onChange={(event) => setValue(event.currentTarget.value)}
+                        />
 
-                            <TextInput
-                                className="pt-4 justify-center pr-96 mr-44"
-                                variant="filled"
-                                placeholder="Enter Repo URL..."
-                                size="xl"
-                                radius="60"
-                                value={value}
-                                onChange={(event) => setValue(event.currentTarget.value)}
-                            />
-
-
-                    <div className="flex items-center gap-3 md:gap-5 ">
-
-                        <button type="button" onClick={() => signOut()} className="outline_btn">
-                            Sign Out
-                        </button>
-                        <p className="text-blue-600">{session.user.name}</p>
-                        <Link href="/profile">
-                            <Image
-                                src="/assets/images/profile.jpg"
-                                alt="profile picture"
-                                width={50}
-                                height={0}
-                                className="rounded-full"
-                            />
-                        </Link>
-                    </div>
+                        <div className="flex items-center gap-3 md:gap-5 ">
+                            <button type="button" onClick={() => signOut()} className="outline_btn">
+                                Sign Out
+                            </button>
+                            <p className="text-blue-600">{session.user.name}</p>
+                            <Link href={`/user/${session.login}`}>
+                                <Image
+                                    src={`${session.user.image}`}
+                                    alt="profile picture"
+                                    width={50}
+                                    height={0}
+                                    className="rounded-full"
+                                />
+                            </Link>
+                        </div>
                     </div>
                 ) : (
                     <div className="flex gap-3 md:gap-5">
