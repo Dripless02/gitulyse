a09a7a036991021b5f3614f2e61baa07ee702547
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2024-04-08 15:08:09 +0100

    Rework contribution parsing for users page using function from commits

--- src/backend/gitulyse_api/users.py
+++ src/backend/gitulyse_api/users.py
@@ -1,10 +1,11 @@
+import logging
 from concurrent.futures import ThreadPoolExecutor
 from time import time
 
 from flask import Blueprint, jsonify, request, current_app
-from github import Auth, Github, BadCredentialsException, GithubException
-from github.Repository import Repository
+from github import Auth, Github, BadCredentialsException
 
+from gitulyse_api.commits import parse_contributions
 from gitulyse_api.db import get_db
 
 bp = Blueprint('users', __name__)
@@ -46,7 +47,7 @@ def parse_repo(repo_user):
 @bp.route("/get-user", methods=["GET"])
 def get_user():
     token = request.args.get("token")
-    user = request.args.get("user")
+    user = request.args.get("user").lower()
     force = request.args.get("force")
 
     db_client = get_db()
@@ -63,8 +64,8 @@ def get_user():
         return jsonify({"message": "Invalid token"}), 401
 
     try:
-        latest_user_info = db_client["users"][user.login].find({}, {"_id": 0}).sort("timestamp", -1).limit(1)[0]
-        if not force and latest_user_info["timestamp"] > time() - 3600:
+        latest_user_info = db_client["users"][user.login.lower()].find({}, {"_id": 0}).sort("timestamp", -1).limit(1)[0]
+        if not force and latest_user_info["timestamp"] > time() - 600:
             return jsonify(latest_user_info), 200
     except IndexError:
         pass
@@ -79,29 +80,62 @@ def get_user():
         "created_at": user.created_at,
         "avatar_url": user.avatar_url,
         "html_url": user.html_url,
-        "contribution_counts": {
-            "additions": 0,
-            "deletions": 0,
-            "commits": 0,
-        },
         "repos": {}
     }
 
     all_user_repos = [repo for repo in user.get_repos(type="all")]
     user_info["repos"] = [{"name": repo.full_name, "html_url": repo.html_url} for repo in all_user_repos]
 
-    start_time = time()
-    with ThreadPoolExecutor(max_workers=10) as repo_executor:
-        contribution_counts = repo_executor.map(parse_repo, [(repo, user.login) for repo in all_user_repos])
-        for repo_contribution_counts in contribution_counts:
-            user_info["contribution_counts"]["additions"] += repo_contribution_counts["additions"]
-            user_info["contribution_counts"]["deletions"] += repo_contribution_counts["deletions"]
-            user_info["contribution_counts"]["commits"] += repo_contribution_counts["commits"]
-    end_time = time()
-    current_app.logger.info(f"Time taken to calculate contributions: {end_time - start_time}")
+    contributions = get_user_contributions(all_user_repos, user)
 
+    user_info["repo_contributions"] = contributions["repo_contributions"]
+    user_info["overall_contributions"] = contributions["overall_contributions"]
     user_info["timestamp"] = time()
-
-    db_client["users"][user.login].insert_one(user_info.copy())
+    db_client["users"][user.login.lower()].insert_one(user_info.copy())
 
     return jsonify(user_info), 200
+
+
+def process_repo(repo, user, contributions_per_repo, overall_contributions):
+    contributions_per_repo[repo.full_name] = {}
+
+    stats_contributors = repo.get_stats_contributors()
+    if stats_contributors is None:
+        log.info(f"{repo.full_name} was None")
+        return
+
+    for week in stats_contributors[0].weeks:
+        week_formatted = week.w.date().strftime("%Y-%m")
+        contributions_per_repo[repo.full_name][week_formatted] = {
+            "additions": 0,
+            "deletions": 0,
+            "commits": 0,
+        }
+        if week_formatted not in overall_contributions:
+            overall_contributions[week_formatted] = {
+                "additions": 0,
+                "deletions": 0,
+                "commits": 0,
+            }
+
+    user_stats_contributor = next(
+        (contributor for contributor in stats_contributors if contributor.author.login == user.login), None)
+
+    with ThreadPoolExecutor(max_workers=2) as parse_executor:
+        parse_executor.submit(parse_contributions, user_stats_contributor, contributions_per_repo[repo.full_name],
+                              True)
+        parse_executor.submit(parse_contributions, user_stats_contributor, overall_contributions, True)
+
+
+def get_user_contributions(all_user_repos, user):
+    contributions_per_repo = {}
+    overall_contributions = {}
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        _ = [executor.submit(process_repo, repo, user, contributions_per_repo, overall_contributions) for repo in
+             all_user_repos]
+
+    return {
+        "repo_contributions": contributions_per_repo,
+        "overall_contributions": overall_contributions
+    }
