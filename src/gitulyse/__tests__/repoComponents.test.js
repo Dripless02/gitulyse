c2ff1433d991fbe21162027fe5d652e2492b56d2
Afolabi <afolabi.fatogun2@mail.dcu.ie>
2024-04-13 17:51:59 +0100

    Add unit tests for repoComponents.test.js

---
+++ src/gitulyse/__tests__/repoComponents.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CodeContributions from '@/components/repo/CodeContributions';
+import IssueTracking from '@/components/repo/IssueTracking';
+import PullRequests from '@/components/repo/PullRequests';
+import PercentagePullrequests from '@/components/repo/PercentagePullrequests';
+import PercentageIssues from '@/components/repo/PercentageIssues';
+
+jest.mock('d3', () => ({
+    __esModule: true,
+    default: jest.fn(),
+  }));
+describe('CodeContributions Components', () => {
+    test('renders the component', () => {
+        render(<CodeContributions />);
+        expect(screen.getByText("Code Contributions per Month")).toBeInTheDocument();
+    });
+});
+
+describe('IssueTracking Components', () => {
+    test('renders the component', () => {
+        render(<IssueTracking />);
+        expect(screen.getByText("Issue Tracking")).toBeInTheDocument();
+    });
+});
+
+describe('PullRequests Components', () => {
+    test('renders the component', () => {
+        render(<PullRequests />);
+        expect(screen.getByText("Time to Merge Pull requests")).toBeInTheDocument();
+    });
+});
+
+// describe('PercentagePullrequests Components', () => {
+//     test('renders the component', () => {
+//         render(<PercentagePullrequests />);
+//         expect(screen.getByText("Percentage of issues resolved/created from")).toBeInTheDocument();
+//     });
+// });
+
+// describe('Percentage Issues Component', () => {
+//     test('renders the component', () => {
+//         render(<PercentageIssues />);
+//         expect(screen.getByText("Percentage of issues resolved/created from")).toBeInTheDocument();
+//     });
+// });
